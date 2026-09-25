b6a3ae149e4d71ebfc3f87538c5e59562dea0d84
0-V-linuxdo <0_v@linux.do>
2026-09-25 15:29:40 +0000

    [20260925.1] Channels: stamp Development header on dev
    Same source as voidpp. This branch's userscript updates from …/dev/.

--- userscript/VoidPP.meta.js
+++ userscript/VoidPP.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Void++
-// @namespace    https://github.com/0-V-linuxdo/VoidPP/voidpp
+// @namespace    https://github.com/0-V-linuxdo/VoidPP/dev
 // @version      20260925.1
 // @description  A modification for grok.com
 // @author       Prism & Void++ Contributors
-// @environment  Beta
+// @environment  Development
 // @homepageURL  https://github.com/0-V-linuxdo/VoidPP
-// @icon         https://raw.githubusercontent.com/0-V-linuxdo/VoidPP/voidpp/assets/logos/app-icon/voidpp-icon.svg
+// @icon         https://raw.githubusercontent.com/0-V-linuxdo/VoidPP/dev/assets/logos/app-icon/voidpp-icon.svg
 // @match        *://grok.com/*
 // @match        *://*.grok-sandbox.com/*
 // @run-at       document-start
@@ -27,6 +27,6 @@
 // @compatible   opera
 // @license      GPL-3.0-or-later
 // @supportURL   https://github.com/0-V-linuxdo/VoidPP
-// @downloadURL  https://raw.githubusercontent.com/0-V-linuxdo/VoidPP/voidpp/userscript/VoidPP.user.js
-// @updateURL    https://raw.githubusercontent.com/0-V-linuxdo/VoidPP/voidpp/userscript/VoidPP.user.js
+// @downloadURL  https://raw.githubusercontent.com/0-V-linuxdo/VoidPP/dev/userscript/VoidPP.user.js
+// @updateURL    https://raw.githubusercontent.com/0-V-linuxdo/VoidPP/dev/userscript/VoidPP.user.js
 // ==/UserScript==
