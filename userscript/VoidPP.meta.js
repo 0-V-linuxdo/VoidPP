8d4826fca0375f4da092b776e2b71057753db889
0-V-linuxdo <0_v@linux.do>
2026-09-25 15:39:45 +0000

    [20260925.2] Channels: stamp Production header on voidpp-stable

--- userscript/VoidPP.meta.js
+++ userscript/VoidPP.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Void++
 // @namespace    https://github.com/0-V-linuxdo/VoidPP/voidpp-stable
-// @version      20260925.1
+// @version      20260925.2
 // @description  A modification for grok.com
 // @author       Prism & Void++ Contributors
 // @environment  Production
